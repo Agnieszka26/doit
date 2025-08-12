3ab97f57a4cb7ec8b4097d4800b2e98fd05dc262
Agnieszka26 <medrek.agn@yahoo.com>
2025-08-12 10:19:05 +0200

    feat: bottom sheet

--- src/app/components/drawer/drawer.ts
+++ src/app/components/drawer/drawer.ts
@@ -1,11 +1,93 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ElementRef,
+  HostListener,
+  inject,
+} from '@angular/core';
 
 @Component({
   selector: 'app-drawer',
   imports: [],
   templateUrl: './drawer.html',
-  styleUrl: './drawer.css'
+  styleUrl: './drawer.css',
+  standalone: true
 })
 export class Drawer {
+  @Input() open = false;
+  @Output() closed = new EventEmitter<void>();
+  private startY = 0;
+  private currentY = 0;
+  private startTime = 0;
+  private dragging = false;
+  private threshold = 100; // Threshold for drag distance [px]
+  private velocityThreshold = 0.5; // Threshold for velocity [px/s]
+  public el = inject(ElementRef);
+  // constructor(private el: ElementRef) {}
+  close() {
+    this.closed.emit();
+  }
 
+  startDrag(event: MouseEvent | TouchEvent) {
+    if (!this.open) return;
+    this.dragging = true;
+    this.startY = this.getY(event);
+    this.currentY = this.startY;
+    this.startTime = Date.now();
+
+    const sheetEl = this.el.nativeElement.querySelector(
+      '.sheet',
+    ) as HTMLElement;
+    sheetEl.style.transition = 'none'; // Disable transition during drag
+  }
+
+  @HostListener('document:mousemove', ['$event'])
+  @HostListener('document:touchmove', ['$event'])
+  onDrag(event: MouseEvent | TouchEvent) {
+    if (!this.dragging) return;
+    this.currentY = this.getY(event);
+    const diff = this.currentY - this.startY;
+    if (diff > 0) {
+      this.setTranslate(diff);
+    }
+  }
+  @HostListener('document:mouseup')
+  @HostListener('document:touchend')
+  endDrag() {
+    if (!this.dragging) return;
+    this.dragging = false;
+    const sheetEl = this.el.nativeElement.querySelector(
+      '.sheet',
+    ) as HTMLElement;
+    sheetEl.style.transition = ''; // Re-enable transition after drag
+    //zamknij jeśli:
+    // - przeciągnięto dalej niż threshold
+    // - lub prędkość przeciągania była większa niż velocityThreshold
+    const diff = this.currentY - this.startY;
+    const elapsed = Date.now() - this.startTime;
+    const velocity = diff / elapsed; // px/ms
+
+    if (diff > this.threshold || velocity > this.velocityThreshold) {
+      this.close();
+    } else {
+      this.setTranslate(0); // Reset position if not closed
+    }
+  }
+  onBackdropKeydown(event: Event) {
+    console.log('xds', event);
+  }
+
+  private getY(event: MouseEvent | TouchEvent): number {
+    return event instanceof MouseEvent
+      ? event.clientY
+      : event.touches[0].clientY;
+  }
+  private setTranslate(y: number) {
+    const sheetEl = this.el.nativeElement.querySelector(
+      '.sheet',
+    ) as HTMLElement;
+    sheetEl.style.transform = `translateY(${y}px)`;
+  }
 }
--- src/app/pages/login/login.ts
+++ src/app/pages/login/login.ts
@@ -1,14 +1,28 @@
 import { Component } from '@angular/core';
 import { LoginForm } from '../../components/forms/login-form/login-form';
 import { texts } from '../../constants/texts';
-import { LinkComponent } from "../../components/atoms/link-component/link-component";
+import { LinkComponent } from '../../components/atoms/link-component/link-component';
+import { Drawer } from '../../components/drawer/drawer';
 
 @Component({
   selector: 'app-login',
-  imports: [LoginForm, LinkComponent],
+  imports: [LoginForm, LinkComponent, Drawer],
   templateUrl: './login.html',
-  styleUrl: './login.css'
+  styleUrl: './login.css',
+  standalone: true,
 })
 export class Login {
-readonly TEXTS = texts.LOGIN_SIGNIN;
+  readonly TEXTS = texts.LOGIN_SIGNIN;
+  isOpen = false;
+  drawerType: 'apple' | 'google' | null = null;
+
+  openSheet(type: 'apple' | 'google') {
+    this.drawerType = type;
+    console.log('openSheet()');
+    this.isOpen = true;
+  }
+
+  closeSheet() {
+    this.isOpen = false;
+  }
 }
