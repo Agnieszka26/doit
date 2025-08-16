3d4a0d91ca700ef55b2aea0be33533cede86dc45
Agnieszka26 <medrek.agn@yahoo.com>
2025-08-16 15:01:08 +0200

    deploy: firebase config

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -3,6 +3,12 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 export const serverRoutes: ServerRoute[] = [
   {
     path: '**',
-    renderMode: RenderMode.Prerender
-  }
+    renderMode: RenderMode.Prerender,
+  },
+  {
+    path: 'task/:id',
+    renderMode: RenderMode.Prerender, // tell Angular which IDs to prerender
+    getPrerenderParams: () =>
+      Promise.resolve([{ id: '1' }, { id: '2' }, { id: '3' }]),
+  },
 ];
